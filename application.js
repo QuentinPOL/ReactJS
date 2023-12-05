c7a0b5c1ef28b3d351196cc05309f630545470b6
QuentinPOL <Quentinpol140@gmail.com>
2023-12-05 10:51:57 +0100

    second commit

--- application.js
+++ application.js
@@ -1,44 +1,51 @@
 const express = require('express');
-const os = require('os');
-//const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
 const app = express();
-const port = 82; // Le port sur lequel votre serveur écoutera
+const port = 8080;
 
-//middleware 
-//app.use(bodyParser.json()); // décode le body d'une requête
-//app.use(bodyParser.urlencoded({ extended: true }));
-
-// Route d'exemple GET
-app.get('/', (req, res) => {
-    //res.send('<h1><strong>Not Found</strong></h1><br>The requested URL was not found on this server.<br><hr>Apache/2.4.56 (Debian) Server at 192.168.65.9 Port 81');
-    const donneeDuCorps = req.query.login;
-    
-    if (donneeDuCorps != '')
-    {
-        console.log(donneeDuCorps);
-        res.send("<p>Donées reçues et traitées !</p>");
-    }
-    else
-    {
-        res.send('<html><head><title>Formulaire de Connexion</title></head><body><form action="" method="get"><label for="login">Login :</label><input type="text" id="login" name="login" required><input type="submit" value="Envoyer"></form></body></html>');
-    }
+// Configuration de la connexion à la base de données MySQL
+const db = mysql.createConnection({
+  host: '192.168.65.9',
+  user: 'root',
+  password: 'root',
+  database: 'Lawrence'
 });
 
-// Route d'exemple POST
-//app.post('/', (req, res) => {
-//    res.send("<p>Donées reçues et traitées !</p>");
-//});
-
-// Démarrer le serveur
-app.listen(port, () => {
-    console.log(`Serveur est en écoute sur ${os.networkInterfaces()['ens18'][0].address}:${port}`);
+// Connexion à la base de données MySQL
+db.connect((err) => {
+  if (err) {
+    console.error('Erreur de connexion à la base de données:', err);
+    throw err;
+  }
+  console.log('Connecté à la base de données MySQL');
 });
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-// Gestionnaire d'erreurs global
-process.on('uncaughtException', (err) => {
-    console.error(`Une erreur non capturée s'est produite : ${err.message}`);
-    process.exit(1); // 1 signifie une sortie avec une erreur
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Requête SQL pour vérifier l'utilisateur dans la base de données
+  const sql = 'SELECT * FROM user WHERE email = ? AND passwd = ?';
+
+  db.query(sql, [username, password], (err, results) => {
+    if (err) {
+      console.error('Erreur de requête SQL:', err);
+      res.json({ success: false, message: 'Erreur de requête SQL' });
+      return;
+    }
+
+    if (results.length > 0) {
+      res.json({ success: true, message: 'Connexion réussie' });
+    } else {
+      res.json({ success: false, message: 'Nom d\'utilisateur ou mot de passe incorrect' });
+    }
   });
-  
+});
+
+app.listen(port, () => {
+  console.log(`Serveur en cours d'exécution sur le port ${port}`);
+});
